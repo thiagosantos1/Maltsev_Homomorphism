8d15ad33cfd4977e72783f2a5a2e74aaba65e293
Thiago Santos <thiagogyn1@hotmail.com>
2019-03-24 23:12:02 -0500

    Sampling

--- src/construct_randG_H.c
+++ src/construct_randG_H.c
@@ -46,25 +46,49 @@ void construct_H(NEW_GRAPHS *op)
   op->num_E_H  = 0;
   op->numVertH = 0;
   int max_degree = 0; // Used to then create the size of matrix H(save space)
-  int degree, last_vert = 0;
-  for(int i =0; i<op->numVertG; i++){
+  int degree, last_vert = 0, i, x, j;
+  int ** G_H; 
+  for(i =0; i<op->numVertG; i++){
     degree = (int) pow(2,op->degrees_g[i]);
     max_degree = degree > max_degree ? degree:max_degree;
     op->numVertH += degree;
   }
 
   // each vertex of G is "assign" in H 2 to the power of degree of that vertice
-  op->graph_h = malloc(op->numVertG * sizeof(int * ));
-  for(int i=0; i<op->numVertG; i++){
+  G_H = malloc(op->numVertG * sizeof(int * ));
+  for(i=0; i<op->numVertG; i++){
     // +1 because one position is to how many vertices for that g
-    op->graph_h[i] = malloc( (max_degree +1 ) * sizeof(int) ); 
-    op->graph_h[i][0] = (int) pow(2,op->degrees_g[i]);
-    for(int x=1; x<=op->graph_h[i][0]; x++){
-      op->graph_h[i][x] = last_vert;
+    G_H[i] = malloc( (max_degree +1 ) * sizeof(int) ); 
+    G_H[i][0] = (int) pow(2,op->degrees_g[i]);
+    for(x=1; x<=G_H[i][0]; x++){
+      G_H[i][x] = last_vert;
       last_vert++;
     }
   }
 
+  op->graph_h = malloc(op->numVertH * sizeof(int *));
+  for(i=0; i<op->numVertH; i++){
+    op->graph_h[i] = malloc(op->numVertH * sizeof(int));
+    memset(op->graph_h[i],0,op->numVertH * sizeof(int) );
+  }
+  op->num_E_H =0;
+  op->degrees_h = malloc(op->numVertH * sizeof(int));
+  memset(op->degrees_h,0,op->numVertH * sizeof(int));
+  // Randomly create H and its edges
+  for(i=0; i< op->numVertG - 1; i++){ // for each vertice of G
+    for(j=1; j<=G_H[i][0]; j++){ // All possible assigns in H for vertice i in G
+      for(x=1; x<=G_H[i+1][0]; x++){ // All possible assign in H for vertice i+1
+        if( ((float)rand() / (float)RAND_MAX) >= op->prob_edgeH ){
+          op->graph_h[G_H[i][j]][G_H[i+1][x]] = 1;
+          op->graph_h[G_H[i+1][x]][G_H[i][j]] = 1;
+          op->num_E_H +=2;
+          op->degrees_h[G_H[i][j]]++;
+          op->degrees_h[G_H[i+1][x]]++;
+
+        }
+      }
+    }
+  }
 }
 
 int main(int argc, char const *argv[])
@@ -72,7 +96,7 @@ int main(int argc, char const *argv[])
   
   NEW_GRAPHS graphs;
 
-  graphs.numVertG = 10;
+  graphs.numVertG = 4;
   graphs.prob_edgeG = 0.5;
   graphs.prob_edgeH = 0.5;
 
